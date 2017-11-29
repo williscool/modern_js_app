bd08b82c509a4999a9df5e4521a77f12ec47e85a
William Harris <wharris@upscalews.com>
2017-11-29 01:29:38 -0800

    fix prod build

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@
 export const WDS_PORT = 7000;
 export const APP_NAME = "Modern JS App";
 
-export const STATIC_PATH = "/static";
+export const STATIC_PATH = "";
 
 export const APP_CONTAINER_CLASS = "js-app";
 export const APP_CONTAINER_SELECTOR = `.${APP_CONTAINER_CLASS}`;
